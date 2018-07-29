cec771767177dd9baa85b3038122ca50cd3f2525
Larry Garfield <larry@garfieldtech.com>
2018-07-29 00:36:18 -0400

    Add before/after listener ordering to the registration-based provider.

--- src/RegisterableListenerProvider.php
+++ src/RegisterableListenerProvider.php
@@ -48,11 +48,54 @@ class RegisterableListenerProvider implements ListenerProviderInterface
     {
         $type = $type ?? $this->getParameterType($listener);
 
-        $id = $this->listeners->addItem(new ListenerEntry($listener, $type), $priority);
-
-        return $id;
+        return $this->listeners->addItem(new ListenerEntry($listener, $type), $priority);
     }
 
+    /**
+     * Adds a listener to trigger before another existing listener.
+     *
+     * Note: The new listener is only guaranteed to come before the specified existing listener. No guarantee is made
+     * regarding when it comes relative to any other listener.
+     *
+     * @param string $pivotId
+     *   The ID of an existing listener.
+     * @param callable $listener
+     *   The listener to register.
+     * @param string|null $type
+     *   The class or interface type of events for which this listener will be registered. If not provided
+     *   it will be derived based on the type hint of the listener.
+     * @return string
+     *   The opaque ID of the listener.  This can be used for future reference.
+     */
+    public function addListenerBefore(string $pivotId, callable $listener, string $type = null) : string
+    {
+        $type = $type ?? $this->getParameterType($listener);
+
+        return $this->listeners->addItemBefore($pivotId, new ListenerEntry($listener, $type));
+    }
+
+    /**
+     * Adds a listener to trigger after another existing listener.
+     *
+     * Note: The new listener is only guaranteed to come after the specified existing listener. No guarantee is made
+     * regarding when it comes relative to any other listener.
+     *
+     * @param string $pivotId
+     *   The ID of an existing listener.
+     * @param callable $listener
+     *   The listener to register.
+     * @param string|null $type
+     *   The class or interface type of events for which this listener will be registered. If not provided
+     *   it will be derived based on the type hint of the listener.
+     * @return string
+     *   The opaque ID of the listener.  This can be used for future reference.
+     */
+    public function addListenerAfter(string $pivotId, callable $listener, string $type = null) : string
+    {
+        $type = $type ?? $this->getParameterType($listener);
+
+        return $this->listeners->addItemAfter($pivotId, new ListenerEntry($listener, $type));
+    }
 
 
 }
--- tests/RegisterableListenerProviderTest.php
+++ tests/RegisterableListenerProviderTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 class RegisterableListenerProviderTest extends TestCase
 {
 
-    public function test_add_ordered_items() : void
+    public function test_add_ordered_listeners() : void
     {
         $p = new RegisterableListenerProvider();
 
@@ -28,6 +28,64 @@ class RegisterableListenerProviderTest extends TestCase
         $p->addListener(function (CollectingEvent $event) {
             $event->add('L');
         }, 0);
+
+        $event = new CollectingEvent();
+
+        foreach ($p->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        $this->assertEquals('CRELL', implode($event->result()));
+    }
+
+    public function test_add_listener_before() : void
+    {
+        $p = new RegisterableListenerProvider();
+
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('E');
+        }, 0);
+        $rid = $p->addListener(function (CollectingEvent $event) {
+            $event->add('R');
+        }, 90);
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('L');
+        }, 0);
+        $p->addListenerBefore($rid, function (CollectingEvent $event) {
+            $event->add('C');
+        });
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('L');
+        }, 0);
+
+        $event = new CollectingEvent();
+
+        foreach ($p->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        $this->assertEquals('CRELL', implode($event->result()));
+    }
+
+    public function test_add_listener_after() : void
+    {
+        $p = new RegisterableListenerProvider();
+
+        $rid = $p->addListener(function (CollectingEvent $event) {
+            $event->add('R');
+        }, 90);
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('L');
+        }, 0);
+        $p->addListenerBefore($rid, function (CollectingEvent $event) {
+            $event->add('C');
+        });
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('L');
+        }, 0);
+        $p->addListenerAfter($rid, function (CollectingEvent $event) {
+            $event->add('E');
+        });
 
         $event = new CollectingEvent();
 
