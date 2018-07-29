6473c965fd8f5a4d523c80c505d22a7d3a0bc8a2
Larry Garfield <larry@garfieldtech.com>
2018-07-28 21:42:49 -0400

    PHP League skeleton.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace League\Skeleton;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
