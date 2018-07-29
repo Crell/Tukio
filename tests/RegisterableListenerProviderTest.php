ba3c4bc765096b90fcb300f4064d1fdd09d887db
Larry Garfield <larry@garfieldtech.com>
2018-07-29 00:44:55 -0400

    Add test for filtering events by type.

--- tests/RegisterableListenerProviderTest.php
+++ tests/RegisterableListenerProviderTest.php
@@ -6,8 +6,40 @@ namespace Crell\Tukio;
 
 use PHPUnit\Framework\TestCase;
 
+class EventOne extends CollectingEvent {}
+
+class EventTwo extends CollectingEvent {}
+
 class RegisterableListenerProviderTest extends TestCase
 {
+
+
+    public function test_only_type_correct_listeners_are_returned() : void
+    {
+        $p = new RegisterableListenerProvider();
+
+        $p->addListener(function (EventOne $event) {
+            $event->add('Y');
+        });
+        $p->addListener(function (CollectingEvent $event) {
+            $event->add('Y');
+        });
+        $p->addListener(function (EventTwo $event) {
+            $event->add('N');
+        });
+        // This class doesn't exist but should not result in an error.
+        $p->addListener(function (NoEvent $event) {
+            $event->add('F');
+        });
+
+        $event = new EventOne();
+
+        foreach ($p->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        $this->assertEquals('YY', implode($event->result()));
+    }
 
     public function test_add_ordered_listeners() : void
     {
