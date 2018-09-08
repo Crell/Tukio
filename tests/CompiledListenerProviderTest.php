bcc329467497e941168a07d2e67c3a01df1773b8
Larry Garfield <larry@garfieldtech.com>
2018-09-08 17:27:01 -0500

    Refactor compiled provider tests to be more DRY.

--- tests/CompiledListenerProviderTest.php
+++ tests/CompiledListenerProviderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Crell\Tukio;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
 function listenerA(CollectingTask $event) : void
@@ -59,23 +60,7 @@ class CompiledEventDispatcherTest extends TestCase
         $builder->addListener([Listen::class, 'listen']);
         $builder->addListenerService('D', 'listen', CollectingTask::class);
 
-        try {
-            // Write the generated compiler out to a temp file.
-            $filename = tempnam(sys_get_temp_dir(), 'compiled');
-            $out = fopen($filename, 'w');
-            $compiler->compile($builder, $out, $class, $namespace);
-            fclose($out);
-
-            // Now include it.  If there's a parse error PHP will throw a ParseError and PHPUnit will catch it for us.
-            include($filename);
-
-            /** @var ListenerProviderInterface $provider */
-            $compiledClassName = "$namespace\\$class";
-            $provider = new $compiledClassName($container);
-        }
-        finally {
-            unlink($filename);
-        }
+        $provider = $this->makeProvider($builder, $container, $class, $namespace);
 
         $event = new CollectingTask();
         foreach ($provider->getListenersForEvent($event) as $listener) {
@@ -102,17 +87,44 @@ class CompiledEventDispatcherTest extends TestCase
         $namespace = 'Test\\Space';
 
         $builder = new ProviderBuilder();
-        $compiler = new ProviderCompiler();
-
         $container = new MockContainer();
-
         $subscriber = new MockSubscriber();
 
         $container->addService('subscriber', $subscriber);
-
         $builder->addSubscriber(MockSubscriber::class, 'subscriber');
 
+        $provider = $this->makeProvider($builder, $container, $class, $namespace);
+
+        $event = new CollectingTask();
+        foreach ($provider->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        $this->assertEquals('BCAEDF', implode($event->result()));
+    }
+
+    /**
+     * Converts a builder into a compiled container.
+     *
+     * Technically this is the core of every test in this class.  However, the build process
+     * is identical in all cases so there's little point in repeating the code.
+     *
+     * @param ProviderBuilder $builder
+     *   A builder, ready to compile.
+     * @param ContainerInterface $container
+     *   A container to provide to the built provider.
+     * @param string $class
+     *   The class name of the provider to generate.
+     * @param string $namespace
+     *   The namespace of the provider to generate.
+     * @return ListenerProviderInterface
+     *   The compiled provider.
+     */
+    protected function makeProvider(ProviderBuilder $builder, ContainerInterface $container, string $class, string $namespace) : ListenerProviderInterface
+    {
         try {
+            $compiler = new ProviderCompiler();
+
             // Write the generated compiler out to a temp file.
             $filename = tempnam(sys_get_temp_dir(), 'compiled');
             $out = fopen($filename, 'w');
@@ -130,11 +142,6 @@ class CompiledEventDispatcherTest extends TestCase
             unlink($filename);
         }
 
-        $event = new CollectingTask();
-        foreach ($provider->getListenersForEvent($event) as $listener) {
-            $listener($event);
-        }
-
-        $this->assertEquals('BCAEDF', implode($event->result()));
+        return $provider;
     }
 }
