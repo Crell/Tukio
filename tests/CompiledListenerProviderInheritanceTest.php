c843d17f012dc901101a73a910c6c515fae13938
Larry Garfield <larry@garfieldtech.com>
2018-09-18 21:52:15 -0500

    Add tests to ensure the compiled container handles event inheritance correctly.

---
+++ tests/CompiledListenerProviderInheritanceTest.php
@@ -0,0 +1,152 @@
+<?php
+declare(strict_types=1);
+
+namespace Crell\Tukio;
+
+use PHPUnit\Framework\TestCase;
+use Psr\EventDispatcher\EventInterface;
+
+interface EventParentInterface extends EventInterface
+{
+    public function add(string $val) : void;
+    public function result() : array;
+}
+
+class ListenedDirectly implements EventParentInterface {
+    protected $out = [];
+
+    public function add(string $val) : void
+    {
+        $this->out[] = $val;
+    }
+
+    public function result() : array
+    {
+        return $this->out;
+    }
+}
+
+class Subclass extends ListenedDirectly {}
+
+class NotListenedDirectly implements EventParentInterface {
+    protected $out = [];
+
+    public function add(string $val) : void
+    {
+        $this->out[] = $val;
+    }
+
+    public function result() : array
+    {
+        return $this->out;
+    }
+}
+
+function inheritanceListenerA(EventParentInterface $event) : void
+{
+    $event->add('A');
+}
+
+function inheritanceListenerB(ListenedDirectly $event) : void
+{
+    $event->add('B');
+}
+
+function inheritanceListenerC(Subclass $event) : void
+{
+    $event->add('C');
+}
+
+
+class CompiledListenerProviderInheritanceTest extends TestCase
+{
+    use MakeCompiledProviderTrait;
+
+    public function test_interface_listener_catches_everything() : void
+    {
+        $class = __FUNCTION__;
+        $namespace = 'Test\\Space';
+
+        $builder = new ProviderBuilder();
+        $container = new MockContainer();
+
+        $ns = __NAMESPACE__;
+        $builder->addListener("{$ns}\\inheritanceListenerA");
+
+        $provider = $this->makeProvider($builder, $container, $class, $namespace);
+
+        $tests = [
+            ListenedDirectly::class => 'A',
+            Subclass::class => 'A',
+            NotListenedDirectly::class => 'A',
+        ];
+
+        foreach ($tests as $class => $result) {
+            /** @var EventParentInterface $event */
+            $event = new $class();
+            foreach ($provider->getListenersForEvent($event) as $listener) {
+                $listener($event);
+            }
+            $this->assertEquals($result, implode($event->result()));
+        }
+    }
+
+    public function test_class_listener_catches_subclass() : void
+    {
+        $class = __FUNCTION__;
+        $namespace = 'Test\\Space';
+
+        $builder = new ProviderBuilder();
+        $container = new MockContainer();
+
+        $ns = __NAMESPACE__;
+        $builder->addListener("{$ns}\inheritanceListenerB");
+
+        $provider = $this->makeProvider($builder, $container, $class, $namespace);
+
+        $tests = [
+            ListenedDirectly::class => 'B',
+            Subclass::class => 'B',
+            NotListenedDirectly::class => '',
+        ];
+
+        foreach ($tests as $class => $result) {
+            /** @var EventParentInterface $event */
+            $event = new $class();
+            foreach ($provider->getListenersForEvent($event) as $listener) {
+                $listener($event);
+            }
+            $this->assertEquals($result, implode($event->result()));
+        }
+    }
+
+    public function test_subclass_listener_catches_subclass() : void
+    {
+        $class = __FUNCTION__;
+        $namespace = 'Test\\Space';
+
+        $builder = new ProviderBuilder();
+        $container = new MockContainer();
+
+        $ns = __NAMESPACE__;
+        $builder->addListener("{$ns}\inheritanceListenerC");
+
+        $provider = $this->makeProvider($builder, $container, $class, $namespace);
+
+        $tests = [
+            ListenedDirectly::class => '',
+            Subclass::class => 'C',
+            NotListenedDirectly::class => '',
+        ];
+
+        foreach ($tests as $class => $result) {
+            /** @var EventParentInterface $event */
+            $event = new $class();
+            foreach ($provider->getListenersForEvent($event) as $listener) {
+                $listener($event);
+            }
+            $this->assertEquals($result, implode($event->result()));
+        }
+    }
+
+}
--- tests/CompiledListenerProviderTest.php
+++ tests/CompiledListenerProviderTest.php
@@ -43,6 +43,8 @@ class ListenService
 
 class CompiledEventDispatcherTest extends TestCase
 {
+    use MakeCompiledProviderTrait;
+
     function test_compiled_provider_triggers_in_order()
     {
         $class = 'CompiledProvider';
@@ -129,45 +131,4 @@ class CompiledEventDispatcherTest extends TestCase
     }
 
 
-    /**
-     * Converts a builder into a compiled container.
-     *
-     * Technically this is the core of every test in this class.  However, the build process
-     * is identical in all cases so there's little point in repeating the code.
-     *
-     * @param ProviderBuilder $builder
-     *   A builder, ready to compile.
-     * @param ContainerInterface $container
-     *   A container to provide to the built provider.
-     * @param string $class
-     *   The class name of the provider to generate.
-     * @param string $namespace
-     *   The namespace of the provider to generate.
-     * @return ListenerProviderInterface
-     *   The compiled provider.
-     */
-    protected function makeProvider(ProviderBuilder $builder, ContainerInterface $container, string $class, string $namespace) : ListenerProviderInterface
-    {
-        try {
-            $compiler = new ProviderCompiler();
-
-            // Write the generated compiler out to a temp file.
-            $filename = tempnam(sys_get_temp_dir(), 'compiled');
-            $out = fopen($filename, 'w');
-            $compiler->compile($builder, $out, $class, $namespace);
-            fclose($out);
-
-            // Now include it.  If there's a parse error PHP will throw a ParseError and PHPUnit will catch it for us.
-            include($filename);
-
-            /** @var ListenerProviderInterface $provider */
-            $compiledClassName = "$namespace\\$class";
-            $provider = new $compiledClassName($container);
-        }
-        finally {
-            unlink($filename);
-        }
-
-        return $provider;
-    }
 }
