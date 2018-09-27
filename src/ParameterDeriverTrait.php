6753eb25f3ce37f539b678b528bce2d8e2293e12
Larry Garfield <larry@garfieldtech.com>
2018-09-27 13:09:54 -0500

    Add documentation.

--- src/ParameterDeriverTrait.php
+++ src/ParameterDeriverTrait.php
@@ -52,22 +52,48 @@ trait ParameterDeriverTrait
         return $type;
     }
 
+    /**
+     * Determines if a callable represents a function.
+     *
+     * Or at least a reasonable approximation, since a function name may not be defined yet.
+     *
+     * @param callable $callable
+     * @return True if the callable represents a function, false otherwise.
+     */
     protected function isFunctionCallable(callable $callable) : bool
     {
         // We can't check for function_exists() because it may be included later by the time it matters.
         return is_string($callable);
     }
 
+    /**
+     * Determines if a callable represents a closure/anonymous function.
+     *
+     * @param callable $callable
+     * @return True if the callable represents a closure object, false otherwise.
+     */
     protected function isClosureCallable(callable $callable) : bool
     {
         return $callable instanceof \Closure;
     }
 
+    /**
+     * Determines if a callable represents a method on an object.
+     *
+     * @param callable $callable
+     * @return True if the callable represents a method object, false otherwise.
+     */
     protected function isObjectCallable(callable $callable) : bool
     {
         return (is_array($callable) && is_object($callable[0]));
     }
 
+    /**
+     * Determines if a callable represents a static class method.
+     *
+     * @param callable $callable
+     * @return True if the callable represents a static method, false otherwise.
+     */
     protected function isClassCallable(callable $callable) : bool
     {
         return (is_array($callable) && is_string($callable[0]) && class_exists($callable[0]));
