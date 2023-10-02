e22cba6f433698ee2c7700a781b021ca61e51431
Larry Garfield <larry@garfieldtech.com>
2023-10-01 19:56:45 -0500

    PHPStan Level 8.

--- src/ListenerPriority.php
+++ src/ListenerPriority.php
@@ -9,7 +9,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class ListenerPriority extends Listener
 {
-    public function __construct(?int $priority, ?string $id = null, ?string $type = null) {
+    public function __construct(int $priority, ?string $id = null, ?string $type = null) {
         parent::__construct(id: $id, order: Order::Priority($priority), type: $type);
     }
 }
